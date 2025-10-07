65b8bfb246e5f8ccca421d8e6feb1652e09367d2
Aanushka Singh <aanushkasingh328@gmail.com>
2025-10-07 17:50:41 +0530

    Updated serverjs.

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(json());
 
-const productsFilePath = join(process.cwd(), 'server', 'products.json');
+const productsFilePath = join(process.cwd(), 'server', 'Products.json');
 
 let products = [];
 let orderCounter = 1;
